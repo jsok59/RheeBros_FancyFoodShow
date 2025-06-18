b24b12874eb989b6445a8abac3d3f66d72339554
John Sok <98001557+jsok59@users.noreply.github.com>
2025-06-18 09:46:46 -0400

    Finish intro page...hopefully

--- webpack.common.js
+++ webpack.common.js
@@ -32,6 +32,13 @@ module.exports = {
 				test: /\.(png|svg|jpg|jpeg|gif)$/i,
 				type: "asset/resource",
 			},
+			{
+				test: /\.(woff2?|ttf|otf|eot)$/,
+				type: "asset/resource",
+				generator: {
+					filename: "fonts/[name][ext][query]", // optional: keeps font files organized
+				},
+			},
 		],
 	},
 };
